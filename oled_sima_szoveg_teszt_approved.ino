7ca5425e788cafd12cbd6c62428aea3aad623eac
kosztyuxo <137617367+Martinkosztyu@users.noreply.github.com>
2026-02-17 08:39:44 +0100

    Update oled_sima_szoveg_teszt_approved.ino

--- oled_sima_szoveg_teszt_approved.ino
+++ oled_sima_szoveg_teszt_approved.ino
@@ -4,8 +4,8 @@
 
 U8G2_SH1106_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-const int trigPin = 9;  // Digitális 9
-const int echoPin = 10; // Digitális 10
+const int trigPin = 9;  // 
+const int echoPin = 10; //
 
 void setup() {
   pinMode(trigPin, OUTPUT);
@@ -14,7 +14,7 @@ void setup() {
 }
 
 void loop() {
-  // Mérés
+  
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -24,7 +24,6 @@ void loop() {
   long duration = pulseIn(echoPin, HIGH);
   int distance = duration * 0.034 / 2;
 
-  // Korlátozzuk a távolságot a kijelző kedvéért (max 50cm)
   if (distance > 50) distance = 50;
 
   u8g2.firstPage();
@@ -32,19 +31,15 @@ void loop() {
     u8g2.setFont(u8g2_font_ncenB10_tr);
     u8g2.drawStr(0, 15, "TAVOLSAG:");
 
-    // 1. Számszerű kiírás
     char buf[10];
     sprintf(buf, "%d cm", distance);
     u8g2.drawStr(80, 15, buf);
 
-    // 2. Vizuális sáv (Minél közelebb, annál vastagabb/nagyobb)
-    // Ha 10cm-nél közelebb van, villogjon egy keret
     if (distance < 10) {
       u8g2.drawFrame(0, 20, 128, 44);
       u8g2.setFont(u8g2_font_ncenB14_tr);
       u8g2.drawStr(30, 48, "STOP!");
     } else {
-      // Egy dinamikus vonal, ami mutatja a távolságot
       int barLen = map(distance, 0, 50, 0, 128);
       u8g2.drawBox(0, 30, barLen, 20);
     }
